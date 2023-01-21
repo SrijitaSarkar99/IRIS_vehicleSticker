abad46795111315790351ff1cefddc7b2860fb04
Aman Dhariwal <a.dhariwal12345@gmail.com>
2023-01-21 15:47:12 +0530

    modified:   client/src/pages/Profile.js 	modified:   client/src/pages/Vehicles.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import { CheckIcon, CloseIcon, EditIcon } from '@chakra-ui/icons';
-import { Avatar, Box, ButtonGroup, Editable, EditableInput, EditablePreview, Flex, FormControl, FormLabel, Heading, HStack, IconButton, Image, Input, Text, useColorModeValue, useEditableControls, VStack } from '@chakra-ui/react'
+import { Avatar, Box, ButtonGroup, Divider, Editable, EditableInput, EditablePreview, Flex, FormControl, FormLabel, Heading, HStack, IconButton, Image, Input, Text, useColorModeValue, useEditableControls, VStack } from '@chakra-ui/react'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Form } from 'react-router-dom';
@@ -27,6 +27,7 @@ function EditableControls() {
 }
 
 function Profile() {
+  
   const [userProfile,setuserProfile] = useState([]);
   const currentUser = JSON.parse(localStorage.getItem("user"));
   const titleColor = useColorModeValue("teal.300", "teal.200");
@@ -60,17 +61,59 @@ function Profile() {
               mt='10px'
               mb='26px'
               marginTop={3}>User Profile </Heading>
-   <Box maxW={'50rem'} borderWidth='1px' borderRadius='lg' overflow='hidden' justifyContent={'center'} p='10'
+   <Box 
+  //  w='1080px'
+   maxW={'50rem'}
+  // position="absolute"
+    borderWidth='1px' borderRadius='lg' overflow='hidden' justifyContent={'center'} p='10'
    boxShadow="0 20px 27px 0 rgb(0 0 0 / 5%)"
    >
+    <HStack spacing='25rem'>
    <Avatar size='2xl' name={userProfile.name} src='https://bit.ly/sage-adebayo' />{' '}
-   <Text> {userProfile.name} </Text>
-    <FormControl>
+<Text>Profile:  {userProfile.status}</Text>
+   {/* <FormControl 
+     mt='1rem'
+     mb='1rem'
+    >
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Profile Status</FormLabel>
+    <Input
+    fontSize="sm"
+    width='auto'
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="status"
+    mb="24px"
+    size="lg"
+    name="status"
+    variant='filled'
+    value={userProfile.status}
+    disabled
+    />
+    </HStack>
+    </FormControl> */}
+    </HStack>
+<Divider
+mt={'1rem'}
+/>
+<Heading
+size={'sm'}
+color={titleColor}
+mt='1rem'
+>
+  Personal Details
+</Heading>
+   <HStack>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Name </FormLabel>
     <Input
     fontSize="sm"
     ms="4px"
+    width='auto'
     borderRadius="15px"
     type="text"
     id="name  "
@@ -79,42 +122,51 @@ function Profile() {
     name="name"
     variant='filled'
     placeholder={userProfile.name}
+    disabledInputStyle={{opacity: 1}}
     disabled
     />
     </HStack>
     </FormControl>
 
-    <FormControl>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Email </FormLabel>
     <Input
     fontSize="sm"
+    width='sm'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="email  "
     mb="24px"
     size="lg"
-    name="name"
+    name="email"
     variant='filled'
     placeholder={userProfile.email}
     disabled
     />
     </HStack>
     </FormControl>
-   
-    <FormControl>
+    </HStack>
+
+<HStack>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Aadhar Number </FormLabel>
     <Input
     fontSize="sm"
     ms="4px"
+    width='auto'
     borderRadius="15px"
     type="number"
-    id="name  "
+    id="aadhar"
     mb="24px"
     size="lg"
-    name="name"
+    name="aadhar"
     variant='filled'
     placeholder={userProfile.aadhar_number}
     disabled
@@ -122,37 +174,43 @@ function Profile() {
     </HStack>
     </FormControl>
 
-    <FormControl>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Mobile Number </FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="number"
-    id="name  "
+    id="mobile  "
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="mobile"
     placeholder={userProfile.mobile_number}
-    disabled
     />
     </HStack>
     </FormControl>
+    </HStack>
 
-    <FormControl>
+<HStack>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'> Gender </FormLabel>
     <Input
+    width='auto'
     fontSize="sm"
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="gender"
     mb="24px"
     size="lg"
-    name="name"
+    name="gender"
     variant='filled'
     placeholder={userProfile.gender}
     disabled
@@ -160,37 +218,45 @@ function Profile() {
     </HStack>
     </FormControl>
 
-    <FormControl>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Department </FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="department"
     mb="24px"
     size="lg"
-    name="name"
+    name="department"
     variant='filled'
     placeholder={userProfile.department}
     disabled
     />
     </HStack>
     </FormControl>
+    </HStack>
 
-    <FormControl>
+<HStack>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Type of Applicant </FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="type"
     mb="24px"
     size="lg"
-    name="name"
+    name="type"
     variant='filled'
     placeholder={userProfile.type}
     disabled
@@ -198,152 +264,164 @@ function Profile() {
     </HStack>
     </FormControl>
 
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Id proof</FormLabel>
+    <Input
+    fontSize="sm"
+    width='auto'
+    pt={'3'}
+    ms="4px"
+    borderRadius="15px"
+    type='file'
+    accept='image/*'
+    id="idproof"
+    mb="24px"
+    size="lg"
+    name="idproof"
+    placeholder={userProfile.id_proof}
+    />
+    </HStack>
+    </FormControl>
+    </HStack>
 
-    <FormControl>
+  <Divider/>
+<Heading
+size={'sm'}
+color={titleColor}
+mt='1rem'
+>
+  Permanent Address 
+</Heading>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Address Line 1</FormLabel>
     <Input
     fontSize="sm"
+    width='lg'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="addressline1"
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="addressline1"
     value={userProfile.address_line1}
     />
     </HStack>
     </FormControl>
 
-    <FormControl>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Address Line 2</FormLabel>
     <Input
     fontSize="sm"
+    width='lg'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="addressline2"
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="addressline2"
     value={userProfile.address_line2}
     />
     </HStack>
     </FormControl>
 
-    <FormControl>
+<HStack>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>City</FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="city  "
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="city"
     value={userProfile.city}
     />
     </HStack>
     </FormControl>
 
-    <FormControl>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>State</FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="state  "
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="state"
     value={userProfile.state}
     />
     </HStack>
     </FormControl>
-   
-    <FormControl>
+    </HStack>
+
+<HStack>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Pincode</FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="pincode"
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="pincode"
     value={userProfile.pin_code}
     />
     </HStack>
     </FormControl>
 
-    <FormControl>
+    <FormControl
+     mt='1rem'
+     mb='1rem'>
     <HStack>
     <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Country</FormLabel>
     <Input
     fontSize="sm"
+    width='auto'
     ms="4px"
     borderRadius="15px"
     type="text"
-    id="name  "
+    id="country"
     mb="24px"
     size="lg"
-    name="name"
-    variant='filled'
+    name="country"
     value={userProfile.country}
     />
     </HStack>
     </FormControl>
-
-    <FormControl>
-    <HStack>
-    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Profile Status</FormLabel>
-    <Input
-    fontSize="sm"
-    ms="4px"
-    borderRadius="15px"
-    type="text"
-    id="name  "
-    mb="24px"
-    size="lg"
-    name="name"
-    variant='filled'
-    value={userProfile.status}
-    disabled
-    />
     </HStack>
-    </FormControl>
+<Divider/>
+    
 
-    <FormControl>
-    <HStack>
-    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Id proof</FormLabel>
-    <Input
-    fontSize="sm"
-    ms="4px"
-    borderRadius="15px"
-    type='file'
-    accept='image/*'
-    id="name  "
-    mb="24px"
-    size="lg"
-    name="name"
-    variant='filled'
-    placeholder={userProfile.id_proof}
-    />
-    </HStack>
-    </FormControl>
+    
    </Box>
    </Box>
    </Flex>
