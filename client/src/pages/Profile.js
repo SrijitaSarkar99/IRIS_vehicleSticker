e584fe6909d0eef0a772350500fcc2075905bb87
Aman Dhariwal <a.dhariwal12345@gmail.com>
2023-01-21 10:16:21 +0530

    modified:   client/src/pages/Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,36 @@
-import { Editable, EditablePreview, HStack, Text, VStack } from '@chakra-ui/react'
+import { CheckIcon, CloseIcon, EditIcon } from '@chakra-ui/icons';
+import { Avatar, Box, ButtonGroup, Editable, EditableInput, EditablePreview, Flex, FormControl, FormLabel, Heading, HStack, IconButton, Image, Input, Text, useColorModeValue, useEditableControls, VStack } from '@chakra-ui/react'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { Form } from 'react-router-dom';
 import AdminNav from "../components/AdminNav"
+
+  
+function EditableControls() {
+  const {
+    isEditing,
+    getSubmitButtonProps,
+    getCancelButtonProps,
+    getEditButtonProps,
+  } = useEditableControls()
+
+  return isEditing ? (
+    <ButtonGroup justifyContent='center' size='sm'>
+      <IconButton icon={<CheckIcon />} {...getSubmitButtonProps()} />
+      <IconButton icon={<CloseIcon />} {...getCancelButtonProps()} />
+    </ButtonGroup>
+  ) : (
+    <Flex justifyContent='center'>
+      <IconButton size='sm' icon={<EditIcon />} {...getEditButtonProps()} />
+    </Flex>
+  )
+}
 
 function Profile() {
   const [userProfile,setuserProfile] = useState([]);
   const currentUser = JSON.parse(localStorage.getItem("user"));
+  const titleColor = useColorModeValue("teal.300", "teal.200");
+
   useEffect(() => {
     async function fetchData(){
     try {
@@ -25,92 +50,303 @@ function Profile() {
   return (
    <>
    <AdminNav/>
-   <VStack>
+   
+   <Flex width="full" align="center" justifyContent="center">
+  <Box>
+   <Heading
+              w='100%'
+              color={titleColor}
+              fontWeight='bold'
+              mt='10px'
+              mb='26px'
+              marginTop={3}>User Profile </Heading>
+   <Box maxW={'50rem'} borderWidth='1px' borderRadius='lg' overflow='hidden' justifyContent={'center'} p='10'
+   boxShadow="0 20px 27px 0 rgb(0 0 0 / 5%)"
+   >
+   <Avatar size='2xl' name={userProfile.name} src='https://bit.ly/sage-adebayo' />{' '}
+   <Text> {userProfile.name} </Text>
+    <FormControl>
     <HStack>
-    <Text>Name:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.name}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Name </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.name}
+    disabled
+    />
+    </HStack>
+    </FormControl>
 
-   <HStack>
-    <Text>Email:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.email}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack>
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Email </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.email}
+    disabled
+    />
+    </HStack>
+    </FormControl>
+   
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Aadhar Number </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="number"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.aadhar_number}
+    disabled
+    />
+    </HStack>
+    </FormControl>
 
-   <HStack>
-    <Text>Phone Number:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.mobile_number}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack>
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Mobile Number </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="number"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.mobile_number}
+    disabled
+    />
+    </HStack>
+    </FormControl>
 
-   <HStack>
-    <Text>Pincode:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.pin_code}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack>
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'> Gender </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.gender}
+    disabled
+    />
+    </HStack>
+    </FormControl>
 
-   <HStack>
-    <Text>Address Line 1:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.addressLine1}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack>
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Department </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.department}
+    disabled
+    />
+    </HStack>
+    </FormControl>
 
-   <HStack>
-    <Text>Address Line 2:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.addressLine2}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack>
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Type of Applicant </FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.type}
+    disabled
+    />
+    </HStack>
+    </FormControl>
 
-   <HStack>
-    <Text>City:</Text>
-    <Editable
-      textAlign='center'
-      value={userProfile.city}
-      fontSize='md'
-      isPreviewFocusable={false}
-    >
-      <EditablePreview />
-   </Editable>
-   </HStack> 
 
-   </VStack>
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Address Line 1</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.address_line1}
+    />
+    </HStack>
+    </FormControl>
+
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Address Line 2</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.address_line2}
+    />
+    </HStack>
+    </FormControl>
+
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>City</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.city}
+    />
+    </HStack>
+    </FormControl>
+
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>State</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.state}
+    />
+    </HStack>
+    </FormControl>
+   
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Pincode</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.pin_code}
+    />
+    </HStack>
+    </FormControl>
+
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Country</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.country}
+    />
+    </HStack>
+    </FormControl>
+
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Profile Status</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type="text"
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    value={userProfile.status}
+    disabled
+    />
+    </HStack>
+    </FormControl>
+
+    <FormControl>
+    <HStack>
+    <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>Id proof</FormLabel>
+    <Input
+    fontSize="sm"
+    ms="4px"
+    borderRadius="15px"
+    type='file'
+    accept='image/*'
+    id="name  "
+    mb="24px"
+    size="lg"
+    name="name"
+    variant='filled'
+    placeholder={userProfile.id_proof}
+    />
+    </HStack>
+    </FormControl>
+   </Box>
+   </Box>
+   </Flex>
    </>
   )
 }
