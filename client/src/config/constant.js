0690a83c2781df7b1777137075f7fdc97cff0a19
Amogh Umesh <amoghumesh02@gmail.com>
2023-02-07 14:25:58 +0000

    minor change

--- client/src/config/constant.js
+++ client/src/config/constant.js
@@ -3,7 +3,7 @@ let BACKEND_SERVER = null;
 if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
-  BACKEND_SERVER = "localhost:5000";
+  BACKEND_SERVER = "http:localhost:5000";
 }
 
 const API_SERVER = BACKEND_SERVER;
